83d2452c619098cac260bf5c6c5a61d49a6d6c82
maxhenschell <mahensch@gmail.com>
2014-03-20 12:35:59 -0500

    Create logexp.R

---
+++ logexp.R
@@ -0,0 +1,18 @@
+# Logistical Exposure Link Function
+# See Shaffer, T.  2004. A unifying approach to analyzing nest success. 
+# Auk 121(2): 526-540.
+library(MASS)
+logexp <- function(exposure = 1)
+{
+  linkfun <- function(mu) qlogis(mu^(1/exposure))
+  linkinv <- function(eta)  plogis(eta)^exposure
+  mu.eta <- function(eta) exposure * plogis(eta)^(exposure-1) *
+    .Call(stats:::C_logit_mu_eta, eta, PACKAGE = "stats")
+  valideta <- function(eta) TRUE
+  link <- paste("logexp(", deparse(substitute(exposure)), ")",
+                sep="")
+  structure(list(linkfun = linkfun, linkinv = linkinv,
+                 mu.eta = mu.eta, valideta = valideta, 
+                 name = link),
+            class = "link-glm")
+}
